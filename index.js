5110b889eee965e8401e94f1ee897a07d5c1ccaf
Paulo Ricardo Xavier Giusti <prxg22@gmail.com>
2019-04-14 01:54:51 -0300

    feat: parse tickets

--- index.js
+++ index.js
@@ -1,46 +1,88 @@
 const puppeteer = require('puppeteer')
 
-const screenshot = async (page, path) => await page.screenshot({ path: `${path}.png` })
-const decolarUrl = 'https://www.decolar.com/shop/flights/search/roundtrip/SAO/NAT/2019-08-01/2019-08-05/2/0/1/NA/NA/NA/NA/NA/?from=SB'
-const resourceUrl = "https://www.decolar.com/shop/flights-busquets/api/v1/web/search"
-const salesRegex = /^https:\/\/www.decolar.com\/shop\/flights-busquets\/api\/v1\/web\/search\?adults./g
+const decolarUrl = 'https://www.decolar.com/shop/flights/search/roundtrip/{from}/{to}/{date1}/{date2}/{adults}/{children}/{babies}/NA/NA/NA/NA/NA/?from=SB'
+const salesRegex = /^https:\/\/www.decolar.com\/shop\/flights-busquets\/api\/v1\/web\/search\?adults=.*/g
 const timeout = 60000
 
-const getTickets = async response => {
-  const type = response.request().resourceType()
+const formatTicketUrl = ({ from, to, date1, date2, adults, children, babies }) => decolarUrl
+  .replace(/{from}/, from)
+  .replace(/{to}/, to)
+  .replace(/{date1}/, date1)
+  .replace(/{date2}/, date2)
+  .replace(/{adults}/, adults)
+  .replace(/{children}/, children)
+  .replace(/{babies}/, babies)
+
+const identifyTicketResponse = response => {
+  const isXHR = response.request().resourceType() === 'xhr'
+
   const url = response.url()
-  const headers = response.headers()
+  const hasTravelUrl = salesRegex.test(url)
 
-  if (
-    !response.ok()   ||
-    ('xhr' !== type) || // allow XHR only
-    !salesRegex.test(url)
-  ) return
+  const ok = response.ok()
 
-
-  const tickets = await response.json()
-  return tickets
+  return (
+    ok &&
+    isXHR && // allow XHR only
+    hasTravelUrl
+  )
 }
 
+const requestTickets = (page, url) => new Promise((resolve, reject) => {
+  page.on('response', async response => {
+    if (!identifyTicketResponse(response)) return
 
-const evaluate = async ({ airport1, airport2, date1, date2 }) => {
-  const browser = await puppeteer.launch()
-  const page = await browser.newPage()
-
-  page.setDefaultTimeout(timeout)
-
-  const res = new Promise((resolve, reject) => {
-    page.on('response', async response => {
-      tickets = await getTickets(response)
-      if (!tickets) return
+    try {
+      const tickets = await response.json()
       resolve(tickets)
-    })
+    } catch (e) {
+      reject(e)
+    }
   })
 
-  await page.goto(decolarUrl, { waitUntil: 'networkidle2' })
-  await browser.close()
-  
-  return res
+  page.goto(url, { waitUntil: 'networkidle2' })
+})
+
+const parseCluster = ({
+  priceDetail: { totalFare: { amount } },
+  routeChoices,
+  airlines,
+}, { referenceData, currency }) => {
+  const [ route1, route2 ] = routeChoices
+
+  const price = `${currency}${amount}`
+  const airline = referenceData.airlines[airlines[0]]
+  const routes = {
+    [route1.type]: route1.routes[route1.selectedRouteIndex],
+    [route2.type]: route2.routes[route2.selectedRouteIndex]
+  }
+
+  return { price , routes , airline }
 }
 
-module.exports = evaluate
+const findBestTicket = (reducer, { arg: { clusters, currencies: [{ mask: currency }], referenceData }}) => {
+  const bestCluster = clusters.find(({ bestCluster: bc }) => bc)
+  if (!bestCluster) return reducer
+  return parseCluster(bestCluster, { referenceData, currency })
+}
+
+const parseTickets = (unparsed) =>  unparsed
+  .reduce(findBestTicket, undefined)
+
+const findTicket = async ({ from, to,  date1, date2, adults, children, babies }) => {
+  const browser = await puppeteer.launch()
+  const page = await browser.newPage()
+  page.setDefaultTimeout(timeout)
+
+  const ticketUrl = formatTicketUrl({ from, to,  date1, date2, adults, children, babies })
+  try {
+    const unparsedTickets = await requestTickets(page, ticketUrl)
+    const ticket = parseTickets(unparsedTickets)
+    return ticket
+  } catch (e) {
+    throw e
+  }
+
+}
+
+module.exports = findTicket
--- index.spec.js
+++ index.spec.js
@@ -2,10 +2,27 @@ const report = require('./index')
 
 jest.setTimeout(60000)
 
+const mock = {
+  from: 'SAO',
+  to: 'NAT',
+  date1: '2019-12-01',
+  date2: '2019-12-06',
+  adults: 2,
+  children: 0,
+  babies: 1,
+}
+
 describe("report", () => {
-  it("should get price", async done => {
-    const tickets = await report({})
-    expect(tickets.length).toBeGreaterThanOrEqual(0)
+  it("should retrieve ticket", async done => {
+    const ticket = await report(mock)
+    expect(ticket).toMatchObject({
+      price: expect.any(String),
+      routes: expect.objectContaining({
+        INBOUND: expect.any(Object),
+        OUTBOUND: expect.any(Object)
+      }),
+      airline: expect.any(String),
+    })
     done();
   })
 })
